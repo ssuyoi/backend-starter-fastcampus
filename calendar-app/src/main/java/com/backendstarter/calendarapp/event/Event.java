e20641b147d3bb2b90662df212cfe169ac02e219
ssuyoi <log.ddua@gmail.com>
2026-04-16 00:10:07 +0900

    feat: 이벤트 등록 및 EventType 별 필터링 로직 추가
    - EventType enum을 정의하여 이벤트 종류를 타입으로 관리
    - 각 이벤트 클래스에 support 메서드 구현으로 EventType 필터링 지원
    - CalendarAppApplication에 이벤트 리스트 필터링 및 출력 로직 추가

--- calendar-app/src/main/java/com/backendstarter/calendarapp/event/Event.java
+++ calendar-app/src/main/java/com/backendstarter/calendarapp/event/Event.java
@@ -3,4 +3,6 @@ package com.backendstarter.calendarapp.event;
 public interface Event {
 
     void print();
+
+    boolean support(EventType type);
 }
---
+++ calendar-app/src/main/java/com/backendstarter/calendarapp/event/EventType.java
@@ -0,0 +1,8 @@
+package com.backendstarter.calendarapp.event;
+
+public enum EventType {
+    MEETING,
+    TO_DO,
+    NO_DISTURBANCE,
+    OUT_OF_OFFICE,
+}
