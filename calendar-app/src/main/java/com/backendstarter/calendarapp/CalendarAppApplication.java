cd5a6a90c6f15b87e3dabe479e1d3b9d18edc372
sua <log.ddua@gmail.com>
2026-04-16 00:49:31 +0900

    feat: CSV 기반 Meeting 데이터 로드 및 처리 기능 추가
    - meeting.csv 파일에서 Meeting 데이터를 읽는 EventCsvReader 클래스 추가
    - OpenCSV 라이브러리를 활용한 CSV 파싱 로직 구현
    - CalendarAppApplication에서 CSV 데이터를 Schedule에 추가하여 출력하도록 변경
    - build.gradle에 OpenCSV 라이브러리 의존성 추가

--- calendar-app/src/main/java/com/backendstarter/calendarapp/CalendarAppApplication.java
+++ calendar-app/src/main/java/com/backendstarter/calendarapp/CalendarAppApplication.java
@@ -6,6 +6,8 @@ import com.backendstarter.calendarapp.event.EventType;
 import com.backendstarter.calendarapp.event.Meeting;
 import com.backendstarter.calendarapp.event.Schedule;
 import com.backendstarter.calendarapp.event.Todo;
+import com.backendstarter.calendarapp.reader.EventCsvReader;
+import java.io.IOException;
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -17,38 +19,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalendarAppApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Schedule schedule = new Schedule();
 
-        HashSet<String> participants = new HashSet<>();
-        participants.add("danny.kim");
+        EventCsvReader csvReader = new EventCsvReader();
+        String meetingCsvPath = "/data/meeting.csv";
 
-        Meeting meeting1 = new Meeting(
-            1, "meeting1",
-            ZonedDateTime.now(), ZonedDateTime.now().plusHours(1),
-            participants, "meetingRoomA", "스터디"
-        );
-        schedule.add(meeting1);
-
-        // 일정 충돌로 인해 등록 x
-        Todo todo1 = new Todo(
-            2, "todo1",
-            ZonedDateTime.now(), ZonedDateTime.now().plusHours(2),
-            "할 일 적기"
-        );
-        schedule.add(todo1);
-
-        // 일정 시작시간보다 일정 종료시간이 빠를 경우 예외 발생
-        Todo todo2 = new Todo(
-            3, "todo2",
-            ZonedDateTime.now().plusHours(5), ZonedDateTime.now().plusHours(4),
-            "할 일 적기"
-        );
-        schedule.add(todo2);
+        List<Meeting> meetings = csvReader.readMeetings(meetingCsvPath);
+        meetings.forEach(schedule::add);
 
         schedule.printAll();
-
-        schedule.printBy(EventType.MEETING);
     }
 
 }
