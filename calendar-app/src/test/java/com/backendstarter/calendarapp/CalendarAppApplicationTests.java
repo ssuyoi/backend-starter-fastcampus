b60f2ed107d1c36b1c653813118833800a0cf085
sua <log.ddua@gmail.com>
2026-04-15 23:35:53 +0900

    chore: initialize 일정 관리 프로그램

---
+++ calendar-app/src/test/java/com/backendstarter/calendarapp/CalendarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstarter.calendarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
