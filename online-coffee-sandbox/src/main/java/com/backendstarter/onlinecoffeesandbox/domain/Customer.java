80107ee3200702153cac76fcd2e10d48362bf9da
ssuyoi <log.ddua@gmail.com>
2026-04-23 22:55:56 +0900

    refactor: Customer 엔티티의 Table 이름 수정
    - 테이블 이름을 customer에서 customers로 변경

--- online-coffee-sandbox/src/main/java/com/backendstarter/onlinecoffeesandbox/domain/Customer.java
+++ online-coffee-sandbox/src/main/java/com/backendstarter/onlinecoffeesandbox/domain/Customer.java
@@ -7,7 +7,7 @@ import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Getter
-@Table(name="customer")
+@Table(name="customers")
 public class Customer {
 
     @Id
