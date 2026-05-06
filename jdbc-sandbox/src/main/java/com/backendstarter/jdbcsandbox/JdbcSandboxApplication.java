ee9e38d8b31a9cd02f6075e7b2b53a3c5f2b5c5f
ssuyoi <log.ddua@gmail.com>
2026-05-07 00:45:20 +0900

    chore: Project 4 실습 프로젝트 생성

---
+++ jdbc-sandbox/src/main/java/com/backendstarter/jdbcsandbox/JdbcSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.backendstarter.jdbcsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbcSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbcSandboxApplication.class, args);
+    }
+
+}
---
+++ jdbc-sandbox/src/test/java/com/backendstarter/jdbcsandbox/JdbcSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstarter.jdbcsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
