f5ed50c543e163608a63f6e83b31cbd663007f70
ssuyoi <log.ddua@gmail.com>
2026-05-11 01:01:25 +0900

    chore: Project 5 Thread 스타일 SNS 실습 프로젝트 생성

---
+++ thread-board/src/main/java/com/backendstarter/threadboard/ThreadBoardApplication.java
@@ -0,0 +1,13 @@
+package com.backendstarter.threadboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadBoardApplication.class, args);
+    }
+
+}
---
+++ thread-board/src/test/java/com/backendstarter/threadboard/ThreadBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstarter.threadboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
