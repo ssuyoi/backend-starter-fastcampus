003e5b64abbb3775bea56d21c341ae1d89f143a4
ssuyoi <log.ddua@gmail.com>
2026-05-14 12:21:47 +0900

    feat: Post API에 로깅 기능 추가 및 application.yml 설정
    - PostController의 모든 엔드포인트에 Logger 적용
    - application.yml에 로깅 수준(root: ERROR) 및 관련 설정 추가

--- thread-board/src/main/java/com/backendstarter/threadboard/controller/PostController.java
+++ thread-board/src/main/java/com/backendstarter/threadboard/controller/PostController.java
@@ -6,6 +6,8 @@ import com.backendstarter.threadboard.model.PostPostRequestBody;
 import com.backendstarter.threadboard.service.PostService;
 import java.util.List;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,11 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/posts")
 public class PostController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PostController.class);
+
     @Autowired
     private PostService postService;
 
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
+        logger.info("GET/ api/v1/posts");
         var posts = postService.getPosts();
         return ResponseEntity.ok(posts);
     }
@@ -34,12 +39,15 @@ public class PostController {
 
     @GetMapping("/{postId}")
     public ResponseEntity<Post> getPostByPostId(@PathVariable Long postId) {
+        logger.info("GET/api/v1/posts/{}", postId);
         var matchingPost = postService.getPostByPostId(postId);
         return ResponseEntity.ok(matchingPost);
     }
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody PostPostRequestBody postPostRequestBody) {
+        logger.info("POST/api/v1/posts");
+
         //변수명만 봐도 타입 추론이 가능한 경우 자동으로 타입을 지정해주는 var를 사용하면 편리함
         var post = postService.createPost(postPostRequestBody);
         return ResponseEntity.ok(post);
@@ -48,12 +56,14 @@ public class PostController {
     @PatchMapping("/{postId}")
     public ResponseEntity<Post> updatePost(@PathVariable Long postId,
         @RequestBody PostPatchRequestBody postPatchRequestBody) {
+        logger.info("PATCH/api/v1/posts/{}", postId);
         var post = postService.updatePost(postId, postPatchRequestBody);
         return ResponseEntity.ok(post);
     }
 
     @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        logger.info("DELETE/api/v1/posts/{}", postId);
         postService.deletePost(postId);
         return ResponseEntity.noContent().build();
     }
