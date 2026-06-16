6cc24a9db5af83aab5fa2f33d569c75df094e46d
ssuyoi <log.ddua@gmail.com>
2026-06-17 00:33:09 +0900

    가상자산 대시보드 API init

---
+++ devops/crypto-dashboard/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    java
+    id("org.springframework.boot") version "4.1.0"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.backendstarter"
+version = "0.0.1-SNAPSHOT"
+description = "crypto-dashboard"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-security-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ devops/crypto-dashboard/src/test/java/com/backendstarter/cryptodashboard/CryptoDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstarter.cryptodashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
