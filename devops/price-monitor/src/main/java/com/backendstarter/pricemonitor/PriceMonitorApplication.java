dff625d5b9e730ea7855385ebac47d619da39028
ssuyoi <log.ddua@gmail.com>
2026-06-16 21:42:11 +0900

    Coinbase 비트코인 달러 조회 서버 init

---
+++ devops/price-monitor/src/main/java/com/backendstarter/pricemonitor/PriceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.backendstarter.pricemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriceMonitorApplication.class, args);
+    }
+
+}
