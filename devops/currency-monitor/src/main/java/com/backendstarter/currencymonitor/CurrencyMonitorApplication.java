433efaecdf26d98c983c7a5dec49ec2c87bd0511
ssuyoi <log.ddua@gmail.com>
2026-06-16 21:48:14 +0900

    한국수출입 은행 환율 조회 서버 init

---
+++ devops/currency-monitor/src/main/java/com/backendstarter/currencymonitor/CurrencyMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.backendstarter.currencymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyMonitorApplication.class, args);
+    }
+
+}
