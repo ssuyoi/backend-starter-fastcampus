33d102fbd7e80d0337dd8797853dde9717ef14f8
ssuyoi <log.ddua@gmail.com>
2026-06-26 23:46:20 +0900

    feat: Spring Security 및 본JPA Auditing 설정 추가
    - @EnableJpaAuditing 애노테이션으로 JPA Auditing 활성화
    - JpaConfig에 AuditorAware 빈 추가로 감사 정보 처리 지원
    - SecurityConfig를 통해 모든 요청 허용 및 기본 로그아웃 설정 구성
      - 이 설정은 인증 구현을 할 때 다시 세팅되어야함

---
+++ test-data/src/main/java/com/backendstarter/testdata/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.backendstarter.testdata.config;
+
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class JpaConfig {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return () -> Optional.of("test");
+    }
+}
