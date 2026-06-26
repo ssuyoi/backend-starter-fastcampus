67080032f36a07b60eb8868cc60f705861639e25
ssuyoi <log.ddua@gmail.com>
2026-06-26 21:33:10 +0900

    init: Spring Boot 프로젝트 초기화 및 Gradle 설정 추가
    - Spring Boot 애플리케이션 초기화
    - Gradle 빌드 스크립트(build.gradle) 및 Wrapper 추가
    - gitignore.io 참고해서 .gitignore 파일 생성
    - 기본 애플리케이션 설정(application.properties) 및 테스트 코드 포함

---
+++ test-data/src/test/java/com/backendstarter/testdata/TestDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendstarter.testdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
