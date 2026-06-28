15de7ac6a447777887da68f074b4ad0adec61df6
ssuyoi <log.ddua@gmail.com>
2026-06-28 16:25:44 +0900

    feat: 가짜 데이터 자료형 enum 설계
    - 자동차 이름과 같은 고유명사처럼 가짜 데이터를 사전식으로 저장해서 관리해야 하는 경우를 도메인으로 설계
    - 데이터 타입, 값으로 구성

---
+++ test-data/src/main/java/com/backendstarter/testdata/domain/MockData.java
@@ -0,0 +1,15 @@
+package com.backendstarter.testdata.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+
+    private String mockDataType;
+    private String mockDataValue;
+
+}
