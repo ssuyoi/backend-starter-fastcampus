e16aa5e5934c218bdec8d8ec48b3bae3fc84cfdd
ssuyoi <log.ddua@gmail.com>
2026-06-30 22:35:47 +0900

    feat: 도메인 클래스(Entity) 추가 및 JPA 설정 반영
    - SchemaField, TableSchema, MockData를 엔티티로 설계하고 JPA 애노테이션 추가
    - 연관 관계 매핑 및 데이터 유효성 검증(nullable, unique 설정) 반영
    - equals, hashCode 메서드에서 ID 기반 비교 로직 구현
    - MockDataType을 포함한 관련 필드/생성자 추가

--- test-data/src/main/java/com/backendstarter/testdata/domain/MockData.java
+++ test-data/src/main/java/com/backendstarter/testdata/domain/MockData.java
@@ -1,15 +1,75 @@
 package com.backendstarter.testdata.domain;
 
+import com.backendstarter.testdata.domain.constant.MockDataType;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 특정 가짜 데이터 자료형에 대응하는 가짜 데이터 알고리즘으로 생성하지 않는 {@link MockDataType}의 경우, 이 가짜 데이터를 랜덤으로 뽑아 출력
+ *
+ * @author sua
+ */
 @Getter
-@Setter
 @ToString
+@Table(uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"mockDataType", "mockDataValue"})
+})
+@Entity
 public class MockData {
 
-    private String mockDataType;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @Column(nullable = false)
+    private MockDataType mockDataType;
+    @Setter
+    @Column(nullable = false)
     private String mockDataValue;
 
+    protected MockData() {
+    }
+
+    public MockData(MockDataType mockDataType, @Nullable String mockDataValue) {
+        this.mockDataType = mockDataType;
+        this.mockDataValue = mockDataValue;
+    }
+
+    public static MockData of(MockDataType mockDataType, @Nullable String mockDataValue) {
+        return new MockData(mockDataType, mockDataValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MockData mockData)) {
+            return false;
+        }
+
+        if (getId() == null) {
+            return Objects.equals(getMockDataType(), mockData.getMockDataType())
+                && Objects.equals(getMockDataValue(), mockData.getMockDataValue());
+        }
+
+        return Objects.equals(getId(), mockData.getId());
+    }
+
+    @Override
+    public int hashCode() {
+
+        if (getId() == null) {
+            return Objects.hash(getMockDataType(), getMockDataValue());
+        }
+        return Objects.hash(getId());
+    }
 }
--- test-data/src/main/java/com/backendstarter/testdata/domain/TableSchema.java
+++ test-data/src/main/java/com/backendstarter/testdata/domain/TableSchema.java
@@ -1,17 +1,106 @@
 package com.backendstarter.testdata.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
-import lombok.EqualsAndHashCode;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 단위 테이블 스키마 정보. 식별자({@link #userId})로 특정할 수 있는 회원이 소유
+ *
+ * @author sua
+ */
 @Getter
-@Setter
-@ToString
-public class TableSchema {
+@ToString(callSuper = true)
+@Entity
+public class TableSchema extends AuditingFields {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
     private String schemaName;
+    @Setter
     private String userId;
+    @Setter
     private LocalDateTime exportedAt;
+
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "tableSchema", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<SchemaField> schemaFields = new LinkedHashSet<>();
+
+    protected TableSchema() {
+    }
+
+    public TableSchema(String schemaName, String userId) {
+        this.schemaName = schemaName;
+        this.userId = userId;
+        this.exportedAt = null;
+    }
+
+    public static TableSchema of(String schemaName, String userId) {
+        return new TableSchema(schemaName, userId);
+    }
+
+    public void markExported() {
+        exportedAt = LocalDateTime.now();
+    }
+
+    public boolean isExported() {
+        return exportedAt != null;
+    }
+
+    public void addSchemaField(SchemaField schemaField) {
+        schemaField.setTableSchema(this);
+        schemaFields.add(schemaField);
+    }
+
+    public void addSchemaFields(Collection<SchemaField> schemaFields) {
+        schemaFields.forEach(this::addSchemaField);
+    }
+
+    public void clearSchemaFields() {
+        schemaFields.clear();
+    }
+
+    /**
+     * Id가 있으면 Id만 비교하고, 없으면 나머지를 전부 비교한다.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TableSchema that)) {
+            return false;
+        }
+
+        if (that.getId() == null) {
+            return Objects.equals(getSchemaName(), that.getSchemaName()) &&
+                Objects.equals(getUserId(), that.getUserId()) &&
+                Objects.equals(getExportedAt(), that.getExportedAt()) &&
+                Objects.equals(getSchemaFields(), that.getSchemaFields());
+        }
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(getSchemaName(), getUserId(), getExportedAt(), getSchemaFields());
+        }
+        return Objects.hash(getId());
+    }
 }
