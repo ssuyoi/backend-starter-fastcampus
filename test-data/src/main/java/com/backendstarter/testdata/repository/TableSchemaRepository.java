69fe787b1410b207e1ee28b6df3594b1d813f79b
ssuyoi <log.ddua@gmail.com>
2026-07-02 00:50:35 +0900

    feat: TableSchemaRepository에 사용자 쿼리 메서드 추가 및 테스트 코드 작성
    - findByUserId, findBySchemaNameAndUserId, deleteBySchemaNameAndUserId 메서드 추가
    
    - 특정 사용자가 가지고 있는 테이블 스키마 전체 목록 조회
    - 특정 사용자와 특정 이름의 단일 스키마 조회
    - 특정 사용자와 특정 이름의 스키마 삭제
    
    - Repository 기능 검증을 위한 단위 테스트 작성 및 검증 완료

--- test-data/src/main/java/com/backendstarter/testdata/repository/TableSchemaRepository.java
+++ test-data/src/main/java/com/backendstarter/testdata/repository/TableSchemaRepository.java
@@ -1,8 +1,14 @@
 package com.backendstarter.testdata.repository;
 
-import com.backendstarter.testdata.domain.MockData;
 import com.backendstarter.testdata.domain.TableSchema;
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface TableSchemaRepository extends JpaRepository<TableSchema, Long> {
+    Page<TableSchema> findByUserId(String userId, Pageable pageable);
+    Optional<TableSchema> findBySchemaNameAndUserId( String schemaName, String userId);
+    void deleteBySchemaNameAndUserId(String schemaName, String userId);
 }
---
+++ test-data/src/test/java/com/backendstarter/testdata/repository/TableSchemaRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.backendstarter.testdata.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.backendstarter.testdata.domain.TableSchema;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
+
+@DisplayName("[Repository] 테이블 스키마 쿼리 테스트")
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+class TableSchemaRepositoryTest {
+
+    @Autowired private TableSchemaRepository tableSchemaRepository;
+
+    @DisplayName("사용자별 테스트 스키마를 조회하면, 페이징된 테이블 스키마를 반환한다.")
+    @Test
+    void givenUserId_whenSelectingTableSchemas_thenReturnPagedTableSchemas() {
+        // given
+        var userId = "djkeh";
+
+        // when
+        Page<TableSchema> result = tableSchemaRepository.findByUserId(userId, Pageable.ofSize(5));
+
+        // then
+        assertThat(result.getContent())
+            .hasSize(1)
+            .extracting("userId", String.class)
+            .containsOnly(userId);
+        assertThat(result.getPageable())
+            .hasFieldOrPropertyWithValue("pageSize", 5)
+            .hasFieldOrPropertyWithValue("pageNumber", 0);
+
+    }
+    @DisplayName("사용자별 테스트 이름을 조회하면,테이블 스키마를 반환한다.")
+    @Test
+    void givenTestName_whenSelectingTableSchema_thenReturnTableSchema() {
+        // given
+        var userId = "djkeh";
+        var schemaName = "test_schema1";
+
+        // when
+        Optional<TableSchema> result = tableSchemaRepository.findBySchemaNameAndUserId(schemaName, userId);
+
+        // then
+        assertThat(result)
+            .get()
+            .hasFieldOrPropertyWithValue("userId", userId)
+            .hasFieldOrPropertyWithValue("schemaName", schemaName);
+
+    }
+    @DisplayName("사용자별 테스트 스키마 이름이 주어지면, 테이블 스키마를 삭제한다.")
+    @Test
+    void givenTestName_whenDeletingTableSchema_thenDeleteTableSchema() {
+        // given
+        var userId = "djkeh";
+        var schemaName = "test_schema1";
+
+        // when
+        tableSchemaRepository.deleteBySchemaNameAndUserId(schemaName, userId);
+
+        // then
+        assertThat(tableSchemaRepository.findBySchemaNameAndUserId(schemaName, userId)).isEmpty();
+
+    }
+}
