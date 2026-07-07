408c39a677126107a8d52e4c47497ea24e91118a
ssuyoi <log.ddua@gmail.com>
2026-07-07 23:53:53 +0900

    refactor: TableSchemaRequest, SchemaFieldRequest를 record에서 class로 변환 (#28)
    - 강의 환경의 스프링 프레임워크 버전에서는 컨트롤러 핸들러 메소드의 입력 파라미터로 `record`를 자유롭게 쓸 수 없어서 기본 생성자와 setter를 갖춘 class로 전환
    - 현재 실습 중인 해당 프로젝트의 스프링 프레임워크 버전에서는 `record` 사용이 가능하나 강의 환경에 맞춰서 연습해봄
    
    - record를 사용하던 요청 DTO를 @Data, @NoArgsConstructor, @AllArgsConstructor 적용한 class로 변경
    - toDto 메서드에서 record의 필드 조회 코드 제거 및 class 필드 직접 참조하도록 수정
    - @AllArgsConstructor 옵션으로 static 메서드 `of` 손쉽게 유지 및 적용

--- test-data/src/main/java/com/backendstarter/testdata/dto/request/SchemaFieldRequest.java
+++ test-data/src/main/java/com/backendstarter/testdata/dto/request/SchemaFieldRequest.java
@@ -2,28 +2,30 @@ package com.backendstarter.testdata.dto.request;
 
 import com.backendstarter.testdata.domain.constant.MockDataType;
 import com.backendstarter.testdata.dto.SchemaFieldDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public record SchemaFieldRequest(
-    String fieldName,
-    MockDataType mockDataType,
-    Integer fieldOrder,
-    Integer blankPercent,
-    String typeOptionJson,
-    String forceValue) {
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@Data
+public class SchemaFieldRequest{
 
-    public static SchemaFieldRequest of(String fieldName, MockDataType mockDataType,
-        Integer fieldOrder, Integer blankPercent, String typeOptionJson, String forceValue) {
-        return new SchemaFieldRequest(fieldName, mockDataType, fieldOrder, blankPercent, typeOptionJson, forceValue);
-    }
+    private String fieldName;
+    private MockDataType mockDataType;
+    private Integer fieldOrder;
+    private Integer blankPercent;
+    private String typeOptionJson;
+    private String forceValue;
 
     public SchemaFieldDto toDto() {
         return SchemaFieldDto.of(
-            fieldName(),
-            mockDataType(),
-            fieldOrder(),
-            blankPercent(),
-            typeOptionJson(),
-            forceValue()
+            fieldName,
+            mockDataType,
+            fieldOrder,
+            blankPercent,
+            typeOptionJson,
+            forceValue
         );
     }
 
