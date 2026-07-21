cc139b2587dfc3460a9e6d6dd837b72edf8aa2d2
ssuyoi <log.ddua@gmail.com>
2026-07-21 23:47:28 +0900

    feat: Github OAuth2 로그인 기능 세팅 (#40)
    - application.yaml에 Github OAuth2 클라이언트 정보 추가
    - 사용자 프로필 데이터에서 주요 속성(id, name, email) 추출 처리
    - SecurityConfig에 Github OAuth2 인증 관련 설정 추가
    - OAuth2UserService 빈 등록 및 GithubUser DTO 매핑 로직 구현

--- test-data/src/main/java/com/backendstarter/testdata/config/SecurityConfig.java
+++ test-data/src/main/java/com/backendstarter/testdata/config/SecurityConfig.java
@@ -3,7 +3,12 @@ package com.backendstarter.testdata.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import com.backendstarter.testdata.dto.security.GithubUser;
 
 @Configuration
 public class SecurityConfig {
@@ -14,5 +19,17 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
             .logout(logout -> logout.logoutSuccessUrl("/"))
             .build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService() {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+
+        return userRequest -> GithubUser.from(
+            // provider 엔드포인트로 HTTP 요청을 보내서 프로필을 받아옴(loadUser)
+            // -> 이 응답을 Map<String, Object> 형태로 꺼냄 (getAttributes)
+            delegate.loadUser(userRequest).getAttributes()
+        );
     }
 }
