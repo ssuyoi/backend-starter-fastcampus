5ab10abdc9195ae6adbb91f0cfdff1b07146cda5
ssuyoi <log.ddua@gmail.com>
2026-07-23 00:09:55 +0900

    feat: 개인 스키마 조회 기능 구현 (#43)
    - loadMySchemas, loadMySchema 메서드에서 페이지네이션 및 단건 조회 지원
    - TableSchemaRepository를 활용한 사용자 테이블 스키마 조회 메서드 구현
    - EntityNotFoundException을 활용해 스키마 조회 실패 시 예외 처리
    - DTO 변환 로직 포함하여 데이터 노출 방식 통합 관리
    - 관련 테스트 작성

---
+++ test-data/src/main/java/com/backendstarter/testdata/service/TableSchemaService.java
@@ -0,0 +1,38 @@
+package com.backendstarter.testdata.service;
+
+import com.backendstarter.testdata.dto.TableSchemaDto;
+import com.backendstarter.testdata.repository.TableSchemaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TableSchemaService {
+
+    private final TableSchemaRepository tableSchemaRepository;
+
+    @Transactional(readOnly = true)
+    public List<TableSchemaDto> loadMySchemas(String userId) {
+
+        return loadMySchemas(userId, Pageable.unpaged()).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<TableSchemaDto> loadMySchemas(String userId, Pageable pageable) {
+        return tableSchemaRepository.findByUserId(userId, pageable)
+            .map(TableSchemaDto::fromEntity);
+    }
+
+    @Transactional(readOnly = true)
+    public TableSchemaDto loadMySchema(String userId, String schemaName) {
+        return tableSchemaRepository.findBySchemaNameAndUserId(schemaName, userId)
+            .map(TableSchemaDto::fromEntity)
+            .orElseThrow(() -> new EntityNotFoundException("테이블 스키마가 없습니다 - userId = " + userId + " , schema = " + schemaName));
+    }
+}
---
+++ test-data/src/test/java/com/backendstarter/testdata/service/TableSchemaServiceTest.java
@@ -0,0 +1,94 @@
+package com.backendstarter.testdata.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import com.backendstarter.testdata.domain.TableSchema;
+import com.backendstarter.testdata.dto.TableSchemaDto;
+import com.backendstarter.testdata.repository.TableSchemaRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+@DisplayName("[Service] 테이블 스키마 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
+class TableSchemaServiceTest {
+
+    @InjectMocks // 주입 대상
+    private TableSchemaService sut;
+    @Mock // 주입할 Mock
+    private TableSchemaRepository tableSchemaRepository;
+
+    @Test
+    void givenUserId_whenLoadingMySchemas_thenReturnsTableSchemaList() {
+        // given
+        String userId = "userId";
+        given(tableSchemaRepository.findByUserId(userId, Pageable.unpaged())).willReturn(new PageImpl<>(
+            List.of(
+            TableSchema.of("table1", userId),
+            TableSchema.of("table2", userId),
+            TableSchema.of("table3", userId)
+        )));
+
+        // when
+        List<TableSchemaDto> result = sut.loadMySchemas(userId);
+
+        // then
+        assertThat(result)
+            .hasSize(3)
+            .extracting("schemaName")
+            .contains("table1", "table2", "table3");
+
+       then(tableSchemaRepository).should().findByUserId(userId, Pageable.unpaged());
+
+
+    }
+
+    @DisplayName("사용자 ID와 스키마 이름이 주어지면, 테이블 스키마를 반환한다.")
+    @Test
+    void givenUserIdAndSchemaName_whenLoadingMySchema_thenReturnsTableSchema() {
+        // Given
+        String userId = "userId";
+        String schemaName = "table1";
+        TableSchema tableSchema = TableSchema.of(schemaName, userId);
+        given(tableSchemaRepository.findBySchemaNameAndUserId(schemaName, userId)).willReturn(
+            Optional.of(tableSchema));
+
+        // When
+        TableSchemaDto result = sut.loadMySchema(userId, schemaName);
+
+        // Then
+        assertThat(result)
+            .hasFieldOrPropertyWithValue("schemaName", schemaName)
+            .hasFieldOrPropertyWithValue("userId", userId);
+        then(tableSchemaRepository).should().findBySchemaNameAndUserId(schemaName, userId);
+    }
+
+    @DisplayName("사용자 ID와 스키마 이름에 대응하는 테이블 스키마가 없으면, 예외를 던진다.")
+    @Test
+    void givenUserIdAndSchemaName_whenLoadingNonexistentMySchema_thenThrowsException() {
+        // Given
+        String userId = "userId";
+        String schemaName = "table1";
+        given(tableSchemaRepository.findBySchemaNameAndUserId(schemaName, userId)).willReturn(Optional.empty());
+
+        // When
+        Throwable t = catchThrowable(() -> sut.loadMySchema(userId, schemaName));
+
+        // Then
+        assertThat(t)
+            .isInstanceOf(EntityNotFoundException.class)
+            .hasMessage("테이블 스키마가 없습니다 - userId: " + userId + ", schemaName: " + schemaName);
+        then(tableSchemaRepository).should().findBySchemaNameAndUserId(schemaName, userId);
+    }
+}
