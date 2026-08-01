5d9a5a1a44afaa75041f7f0e0700cf053d163a87
ssuyoi <log.ddua@gmail.com>
2026-08-02 01:01:34 +0900

    chore: spring-boot-starter-webmvc 의존성으로 교체

--- project-voucher/build.gradle.kts
+++ project-voucher/build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
