b38ed3d837e1d9116e520f119afd1a0c3be69b9a
ssuyoi <log.ddua@gmail.com>
2026-08-01 23:09:37 +0900

    init: Project9 상품권 발권 시스템 Spring Boot 프로젝트 초기 설정 추가
    - Project 9. 상품권 발권 시스템 만들기 강의 실습을 위한 Spring boot 프로젝트 초기 설정

---
+++ project-voucher/src/test/java/com/github/ssuyoi/projectvoucher/ProjectVoucherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ssuyoi.projectvoucher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectVoucherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
